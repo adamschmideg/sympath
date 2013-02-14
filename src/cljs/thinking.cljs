9426e54b7c1e8c6afc609c87c5ca73ad69918ee5
Adam Schmideg <adam@schmideg.net>
2013-02-14 13:33:04 +0100

    Examples of simple field-data calls @0.3

--- src/cljs/thinking.cljs
+++ src/cljs/thinking.cljs
@@ -8,4 +8,25 @@
       :spouse]
     [:children]])
       
+(def db ,,,)
 
+(facts "field-data"
+  (fact
+    (wz/field-data db "/personal/main/name/middle")
+      => {:type :string, :label "Középső név", :value "Archibald"})
+
+  (fact
+    (wz/field-data db "/personal/main/age")
+      => {:type :integer, :label "Életkor", :mandatory true})
+
+  (fact
+    (wz/field-data db "/personal/main/name/prefix")
+      => {:type :string, :label "Előtag", :options ["Dr" "Jr" "Sr"]})
+
+  (fact
+    (wz/field-data db "/personal/main/birthDate")
+      => {:type :map, :label "Születési dátum", :keys ["year" "month" "day"]})
+
+  (fact
+    (wz/field-data db "/personal/main/birthDate/day")
+      => {:type :integer, :range [1 31]}))
