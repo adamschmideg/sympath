3bd04ae2466c0f27668361696e1145644af031f8
Adam Schmideg <adam@schmideg.net>
2013-02-20 09:03:03 +0100

    update project.clj to most recent cljsbuild format @0.8
    --HG--
    rename : src/cljs/thinking.cljs => doc/thinking.cljs

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ A Clojure library designed to ... well, that part is up to you.
 
 FIXME
 
+## Development
+1. Start `lein cljsbuild auto` in a terminal
+2. Start `lein ring server-headless 3000 &`
+3. Start an in-browser REPL
+
+Refreshing the browser will reflect the changes you made to a source
+file.  If you required a namespace, it will be changed to the new
+version.
+
 ## License
 
 Copyright © 2013 FIXME
--- project.clj
+++ project.clj
@@ -6,31 +6,41 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1450"]]
   :source-paths ["src/cljs"]
-  :plugins [[lein-cljsbuild "0.2.7"]]
+  :plugins [[lein-cljsbuild "0.3.0"]
+            [lein-ring "0.7.0"]]
   :hooks [leiningen.cljsbuild]
   :min-lein-version "2.0.0"
-  :cljsbuild {:repl-listen-port     9000
-              :repl-launch-commands {"phantom"       ["phantomjs"
-                                                      "phantom/repl.js"
-                                                      :stdout ".repl-phantom-out"
-                                                      :stderr ".repl-phantom-err"]
-                                     "phantom-naked" ["phantomjs"
-                                                      "phantom/repl.js"
-                                                      "resources/private/html/naked.html"
-                                                      :stdout ".repl-phantom-naked-out"
-                                                      :stderr ".repl-phantom-naked-err"]}
-              :test-commands        {"unit" ["phantomjs"
-                                             "phantom/unit-test.js"
-                                             "resources/private/html/unit-test.html"]}
-              :builds               {:dev  {:source-path "src/cljs"
-                                            :compiler    {:output-to     "resources/public/js/main-debug.js"
-                                                          :optimizations :simple
-                                                          :pretty-print  true}}
-                                     :prod {:source-path "src/cljs"
-                                            :compiler    {:output-to     "resources/public/js/main.js"
-                                                          :optimizations :advanced
-                                                          :pretty-print  false}}
-                                     :test {:source-path "test/cljs"
-                                            :compiler    {:output-to     "resources/private/js/unit-test.js"
-                                                          :optimizations :whitespace
-                                                          :pretty-print  true}}}})
+  :cljsbuild
+    {:builds
+       [{:source-paths ["src/cljs"],
+         :id "dev",
+         :compiler
+         {:pretty-print true,
+          :output-to "resources/public/js/main-debug.js",
+          :optimizations :simple}}
+        {:source-paths ["test/cljs"],
+         :id "test",
+         :compiler
+         {:pretty-print true,
+          :output-to "resources/private/js/unit-test.js",
+          :optimizations :whitespace}}
+        {:source-paths ["src/cljs"],
+         :id "prod",
+         :compiler
+         {:pretty-print false,
+          :output-to "resources/public/js/main.js",
+          :optimizations :advanced}}],
+     :test-commands
+       {"unit"
+        ["phantomjs"
+         "phantom/unit-test.js"
+         "resources/private/html/unit-test.html"]},
+     :repl-listen-port 9000,
+     :repl-launch-commands
+       {"phantom"
+          ["phantomjs" "phantom/repl.js" :stdout ".repl-phantom-out" :stderr ".repl-phantom-err"],
+        "phantom-naked"
+          ["phantomjs" "phantom/repl.js" "resources/private/html/naked.html" :stdout ".repl-phantom-naked-out" :stderr ".repl-phantom-naked-err"],
+        "firefox"
+          ["firefox" :stdout ".repl-firefox-out" :stderr ".repl-firefox-err"]}}
+  :ring {:handler example.routes/app})
