53eb8035e304aee61e94015682f47118d1d90730
Adam Schmideg <adam@schmideg.net>
2013-02-21 08:13:55 +0100

    Starting parse-dictionary-string @0.9

--- src/clj/tree/core.clj
+++ src/clj/tree/core.clj
@@ -1,11 +1,35 @@
 (ns tree.core
   (:use
+    [clojure.string :only [split]]
     [common.impl :only [parse-int]]))
 
+(defn debug
+  [x]
+  (println x)
+  x)
 
 (defn- parse-primitive
   [s]
   (if-let [n (parse-int s)] n s))
+
+(defn- parse-dictionary-string
+  [s]
+  (let [entries (split s #",")]
+    (condp = (count entries)
+      0 :>> {}
+      1 :>>
+        (if (= "*" (first entries))
+          {}
+          (let [[k v] (split (first entries) #"=")]
+            (if v
+              (hash-map (keyword k) (parse-primitive v))
+              k)))
+      (reduce
+        (fn [memo x]
+          (let [[k v] (split x #"=")]
+            (assoc memo (keyword k) (parse-primitive v))))
+        {}
+        entries))))
 
 (defn ^:export add [x y]
   (+ x y))
--- test/clj/test/tree/core.clj
+++ test/clj/test/tree/core.clj
@@ -5,7 +5,8 @@
     [tree.core :only [add]]))
 
 (testable-privates tree.core
-  parse-primitive)
+  parse-primitive
+  parse-dictionary-string)
 
 (facts "About add"
   (fact (add 2 1) => 3))
@@ -17,3 +18,13 @@
   "text" "text"
   "3.5" "3.5" ; float not parsed
   "3" 3)
+
+(tabular
+  (fact "About parse-dictionary-string"
+    (parse-dictionary-string ?str) => ?expected)
+  ?str ?expected
+  "plain" "plain"
+  "num=2" {:num 2}
+  "str=foo" {:str "foo"}
+  "str=foo,num=2" {:str "foo" :num 2}
+  "*" {})
