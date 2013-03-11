4c038de5774bde870e09c4b1b85f524e30365043
Adam Schmideg <adam@schmideg.net>
2013-03-11 14:31:06 +0100

    Workaround for a namespace that is required before it was provided @0.2
    --HG--
    rename : src/cljs/dummy.cljs => src/cljs/zzzdummy.cljs

--- src/cljs/dummy.cljs
+++
@@ -1,6 +0,0 @@
-(ns dummy
-  ; XXX: to make the compiler aware of crossover namespace under clj folder
-  (:require
-    [sympath.core :as core]
-    [sympath.demo :as demo]
-    [sympath.public :as pub]))
---
+++ src/cljs/zzzdummy.cljs
@@ -0,0 +1,9 @@
+(ns zzzdummy
+  ;; XXX: to make the compiler aware of crossover namespace under clj folder
+  ;; I renamed it to start with zzz so its `require` clauses come last.
+  ;; Otherwise `public` was required before it was provided in the
+  ;; generated main-debug.js
+  (:require
+    [sympath.core :as core]
+    [sympath.demo :as demo]
+    [sympath.public :as pub]))
