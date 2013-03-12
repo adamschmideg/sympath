493a55153b472c29909b791452606932d16d7623
Adam Schmideg <adam@schmideg.net>
2013-03-12 10:35:51 +0100

    Demo menu @0.3

--- src/clj/sympath/frontend.clj
+++ src/clj/sympath/frontend.clj
@@ -8,3 +8,24 @@
   [x]
   (to-host-form (core/parse-path x)))
 
+(defn ^:export menu
+  [form]
+  (to-host-form
+    [{:group "customer"
+      :name "personal_info"
+      :label "Personal information"
+      :available true
+      :mandatory true
+      :visited true}
+     {:group "customer"
+      :name "spouse_info"
+      :label "Spouse information"
+      :available false
+      :mandatory false
+      :visited false}
+     {:group "payment"
+      :name "credit_card"
+      :label "Credit card"
+      :available true
+      :mandatory false
+      :visited false}]))
