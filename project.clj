93488c0a242265813deef011ff97f8da2e699f19
Adam Schmideg <adam@schmideg.net>
2013-03-21 13:48:57 +0100

    crossover sources have their own folder @0.4
    --HG--
    rename : src/clj/sympath/core.clj => src/common/sympath/core.clj
    rename : src/clj/sympath/demo.clj => src/common/sympath/demo.clj
    rename : src/clj/sympath/frontend.clj => src/common/sympath/frontend.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1450"]]
-  :source-paths ["src/clj"]
+  :source-paths ["src/common" "src/clj"]
   :test-paths ["test/clj"]
   :plugins [[lein-cljsbuild "0.3.0"]
             [lein-ring "0.7.0"]]
